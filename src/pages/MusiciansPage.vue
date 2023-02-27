3b5036ff4a721c304f4df850ca644720354041d0
Decronus <zick3333@mail.ru>
2023-02-27 03:34:16 +0100

    base

--- src/pages/MusiciansPage.vue
+++ src/pages/MusiciansPage.vue
@@ -1,22 +1,68 @@
 <template>
     <h3 class="q-mb-lg">Исполнители</h3>
-    <q-list bordered>
+    <q-list>
         <q-item
             clickable
-            v-ripple
-            v-for="(musician, index) in $store.state.data.musicians"
+            v-for="(musician, index) in Object.keys(
+                $store.state.data.musicians
+            )"
             :key="index"
+            @click="showSongs(musician)"
         >
-            <q-item-section avatar class="q-mr-none">
+            <q-item-section avatar>
                 <q-icon name="audiotrack" />
             </q-item-section>
             <q-item-section>{{ musician }}</q-item-section>
+            <q-item-section avatar>
+                <q-icon color="primary" name="more_horiz" />
+            </q-item-section>
         </q-item>
     </q-list>
+
+    <q-dialog v-model="icon">
+        <q-layout style="min-height: 0%">
+            <q-card>
+                <q-card-section class="row items-center">
+                    <div class="text-h6">Песни {{ currentMusician }}</div>
+                    <q-space />
+                    <q-btn icon="close" flat round dense v-close-popup />
+                </q-card-section>
+
+                <q-list>
+                    <q-item
+                        v-for="(musician, index) in $store.state.data.musicians[
+                            currentMusician
+                        ]"
+                        :key="index"
+                        @click="icon = true"
+                    >
+                        <q-item-section avatar>
+                            <q-icon name="audiotrack" />
+                        </q-item-section>
+                        <q-item-section>{{ musician }}</q-item-section>
+                    </q-item>
+                </q-list>
+            </q-card>
+        </q-layout>
+    </q-dialog>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            icon: false,
+            currentMusician: null,
+        };
+    },
+
+    methods: {
+        showSongs(musician) {
+            this.currentMusician = musician;
+            this.icon = true;
+        },
+    },
+};
 </script>
 
 <style></style>
